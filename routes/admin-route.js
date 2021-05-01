ff1188e5a1cd5db5c5e9e1cf60f982d5b08141bb
DasPeter <pedrogarcia0108@gmail.com>
2021-05-01 17:35:00 -0500

    POST de admin/profesores

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -1,49 +1,40 @@
-'use strict'
-const router = require('express').Router()
+"use strict";
+const router = require("express").Router();
 const Val = require("../middlewares/validaciones.js");
-const Materia = require('../models/Materia')
-const Carrera = require('../models/Carrera')
+const Materia = require("../models/Materia");
+const Carrera = require("../models/Carrera");
+const Profesor = require("../models/Profesor.js");
 
-router.use(Val.validarToken,Val.validarAdmin);
+router.use(Val.validarToken, Val.validarAdmin);
 
-router.post('/materias',Val.validarCamposMaterias, async (req,res)=>{
-    let {nombre, descripcion,creditos} = req.body;
-    let doc = await Materia.saveMateria({nombre,descripcion,creditos});
-    if(doc){
-        res.status(201).send(doc);
-        return;
-    }
-    res.status(400).send('No se pudo registrar la materia');
-})
+router.post("/materias", Val.validarCamposMaterias, async (req, res) => {
+	let { nombre, descripcion, creditos } = req.body;
+	let doc = await Materia.saveMateria({ nombre, descripcion, creditos });
+	if (doc) {
+		res.status(201).send(doc);
+		return;
+	}
+	res.status(400).send("No se pudo registrar la materia");
+});
 
+router.post("/carreras", Val.validarCamposCarrera, async (req, res) => {
+	let { nombre, descripcion, seriacion } = req.body;
+	let doc = await Carrera.saveCarrera({ nombre, descripcion, seriacion });
+	if (doc) {
+		res.status(201).send(doc);
+		return;
+	}
+	res.status(400).send("No se pudo registrar la carrera");
+});
 
-router.post('/carreras',Val.validarCamposCarrera,async (req,res)=>{
-    let {nombre,descripcion,seriacion} = req.body;
-    let doc = await Carrera.saveCarrera({nombre,descripcion,seriacion})
-    if(doc){
-        res.status(201).send(doc);
-        return;
-    }
-    res.status(400).send('No se pudo registrar la carrera');
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.post("/profesor", Val.validarCamposProfesor, async (req, res) => {
+	let { nombre, apellido } = req.body;
+	let doc = await Profesor.saveProfesor({ nombre, apellido });
+	if (doc) {
+		res.status(201).send(doc);
+		return;
+	}
+	res.status.apply(400).send("No se pudo registrar el profesor");
+});
 
 module.exports = router;
