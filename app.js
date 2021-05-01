c83eaa62f673c567b16ab242d6ad41ad78905ac8
MikeGonzaBar <is722298@iteso.mx>
2021-05-01 12:52:22 -0500

    Added get for carrera

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-'use strict'
+"use strict";
 const express = require("express");
-const alumnoRouter = require('./routes/alumno-route');
-const loginRouter = require('./routes/login-route');
-const materiaRouter = require('./routes/materia-route');
-const adminRouter = require('./routes/admin-route');
+const alumnoRouter = require("./routes/alumno-route");
+const loginRouter = require("./routes/login-route");
+const materiaRouter = require("./routes/materia-route");
+const carreraRouter = require("./routes/carrera-route");
+const adminRouter = require("./routes/admin-route");
 const app = express();
 
 //Borrar lo sigiente tras las PRUEBAS
-const port =  process.env.PORT || 3000;
-app.listen(port, ()=>console.log("Ejecutando en puerto "+port));
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Ejecutando en puerto " + port));
 
 //declaraciones de middleware
-app.use(express.json())
+app.use(express.json());
 
 //relocalizaciones API
-app.use('/api/alumnos', alumnoRouter)
-app.use('/api/login', loginRouter)
-app.use('/api/materias',materiaRouter)
-
+app.use("/api/alumnos", alumnoRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/materias", materiaRouter);
+app.use("/api/carreras", carreraRouter);
 
 //Cosas de admin:
-app.use('/api/admin', adminRouter);
-
-
+app.use("/api/admin", adminRouter);
 
 //relocacizaciones public
-app.use(express.static(__dirname+'/public/html'))
-app.use("/inicio",express.static(__dirname+'/public/html/home.html'))//Historico :3
-
+app.use(express.static(__dirname + "/public/html"));
+app.use("/inicio", express.static(__dirname + "/public/html/home.html")); //Historico :3
--- models/Carrera.js
+++ models/Carrera.js
@@ -1,48 +1,54 @@
-'use strict'
-const mongoose = require('../db/mongodb_connect')
+"use strict";
+const mongoose = require("../db/mongodb_connect");
 
 let carreraSchema = mongoose.Schema({
-    nombre:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    descripcion:{
-        type:String,
-        required:true
-    },
-    seriasion:[{
-        materiaReq:{
-          type:String,
-          required:true
-        },
-        materiaSer:{
-          type:String,
-          required:true
-        }
-    }]
+	nombre: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	descripcion: {
+		type: String,
+		required: true,
+	},
+	seriacion: [
+		{
+			materiaReq: {
+				type: String,
+				required: true,
+			},
+			materiaSer: {
+				type: String,
+				required: true,
+			},
+		},
+	],
 });
 
-carreraSchema.statics.saveCarrera = async function(user){
-    //Suponemos que para este puento el carrera ya fue verificado y cuenta con los atributos necesarios
-    let carrera = new Carrera(user);
-    let doc;
-    try{
-         doc = await carrera.save();
-    }catch(err){
-         doc = undefined;
-    }
-    return doc;
-}
+carreraSchema.statics.saveCarrera = async function (user) {
+	//Suponemos que para este puento el carrera ya fue verificado y cuenta con los atributos necesarios
+	let carrera = new Carrera(user);
+	let doc;
+	try {
+		doc = await carrera.save();
+	} catch (err) {
+		doc = undefined;
+	}
+	return doc;
+};
 
-carreraSchema.statics.getCarrera = async (filtro)=>{
-    return await Carrera.findOne(filtro);
-}
+carreraSchema.statics.getCarreras = async (filtro) => {
+	return await Carrera.find(filtro);
+};
 
-carreraSchema.statics.getCarreraById = async(id)=>{
-    return await Carrera.findById(id);
-}
+carreraSchema.statics.getCarrera = async (filtro) => {
+	return await Carrera.findOne(filtro);
+};
 
-let Carrera = mongoose.model('carrera', carreraSchema);
+carreraSchema.statics.getCarreraById = async (id) => {
+	return await Carrera.findById(id);
+};
+
+let Carrera = mongoose.model("carrera", carreraSchema);
 
 module.exports = Carrera;
