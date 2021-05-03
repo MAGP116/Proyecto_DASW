cc879a6747047a5df4c89ca9fd70641737bbd4d3
MikeGonzaBar <is722298@iteso.mx>
2021-05-03 12:20:25 -0500

    added some stuff on home js

---
+++ js/home.js
@@ -0,0 +1,12 @@
+async function modalUserInfo(token){
+ let user = await fetch(
+  "https://users-dasw.herokuapp.com/api/users/" + email,
+  {
+    method: "GET",
+    headers: {
+      "x-admin": sessionStorage.token,
+      "x-auth": sessionStorage.token,
+    },
+  }
+);
+}
