4f4957f29363d649a5b4a96904acc2465643ce21
MAGP116 <44816914+MAGP116@users.noreply.github.com>
2021-05-04 13:30:06 -0500

    added a get for materias' detail and moved the get materias from career

--- routes/carrera-route.js
+++ routes/carrera-route.js
@@ -7,5 +7,20 @@ router.get('/', async (req,res)=>{
   res.status(200).send(doc)
 })
 
+router.get('/:carrera', async (req,res)=>{
+  let carrera = await Carrera.getCarrera({nombre:req.params.carrera},{_id:0,seriacion:1})
+    if(!carrera){
+        res.status(404).send('No se encontro la carrera');
+        return;
+    }
+    let materias = new Set();
+    carrera.seriacion.forEach(e => {
+        materias.add(e.materiaSer);
+    });
+    materia = Array.from(materias)
+    //FLATA SORTING CHIDO, DE MOMENTO SOLO SON LAS MATERIAS |                       TODO
+    res.status(200).send(materia);
+})
+
 
 module.exports = router;
